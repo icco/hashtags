9f9b8f2617ce570661eb57c38ea7d56f52879adb
Nat Welch <nat@natwelch.com>
2015-05-01 22:36:03 +0000

    use env

--- lib/tweet.rb
+++ lib/tweet.rb
@@ -3,10 +3,10 @@ class Tweet < ActiveRecord::Base
   # Given a hashtag, scrape twitter for more tweets about it.
   def self.get_more hashtag
     client = Twitter::REST::Client.new do |config|
-      config.consumer_key        = "fUl6gF9JYQjzDTDZPdtucRyho"
-      config.consumer_secret     = "oyoYPAghC0NgLGp4YukAdK2jSAeCsmDnBN3SYomPZt3sE4eahd"
-      config.access_token        = "3576561-Jg3VlvSLARfDG6LJFIxT3vgnJGmAiuNSUOgGD8HMLu"
-      config.access_token_secret = "NbPwRrN0frdH0a5vjU08k3gV0muBmBO4iywFWcCIIHOCU"
+      config.consumer_key        = ENV['CONSUMER_KEY']
+      config.consumer_secret     = ENV['CONSUMER_SECRET']
+      config.access_token        = ENV['ACCESS_TOKEN']
+      config.access_token_secret = ENV['ACCESS_TOKEN_SECRET']
     end
 
     client.search("##{hashtag}", result_type: "mixed").each do |tweet|
